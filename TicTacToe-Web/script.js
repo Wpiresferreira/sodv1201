11fde1b2b41969c57447680ca850dbec8f2209be
Wagner Pires Ferreira <125540650+Wpiresferreira@users.noreply.github.com>
2023-07-21 20:01:32 +0000

    First functional version

--- TicTacToe-Web/script.js
+++ TicTacToe-Web/script.js
@@ -1,36 +1,49 @@
-
-
 $("document").ready(function () {
     let player1 = { name: "Player1", token: "X" };
     let player2 = { name: "Player2", token: "O" };
     let board;
     let turn = "X" //P1 = X / P2 = O
+    let hasWinner = ""
 
     $("#player1Name").text(player1.name)
     $("#player2Name").text(player2.name)
     $("#player1Token").text(player1.token)
     $("#player2Token").text(player2.token)
 
-    $("#start").click(function () {
-        console.log("Initializing...")
-        initializeBoard();
-        console.log("Updating Board...")
-        updateBoard();
-        turnMessage();
+
+    start()
+
+    $("#start").click(function(){
+        start()
     });
 
-    $(".cell").click(function(){
+    function start() {
+        hasWinner = ""
+        $("#winner").text("")
+        initializeBoard();
+        updateBoard();
+        turnMessage();
+    }
+
+    $(".cell").click(function () {
         mark(this.id)
     })
 
-    // $("#00").click(function () {
-    // //     console.log("Initializing...")
-     function mark(n) {
-       
-         board[0][0] = turn;
-         updateBoard()
-         changeTurn()
-     }
+    function mark(n) {
+        let row = parseInt(n.substring(0, 1));
+        let col = parseInt(n.substring(1, 2));
+
+        if (hasWinner != "") {
+            return
+        }
+
+        if (board[row][col] == "") {
+            board[row][col] = turn;
+            updateBoard()
+            checkWinner()
+            if(hasWinner == "") changeTurn()
+        }
+    }
 
     function turnMessage() {
         console.log("Turning message " + turn);
@@ -38,7 +51,7 @@ $("document").ready(function () {
         if (turn == "X") $("#turnMessage").text("Player 1 Turn")
         else $("#turnMessage").text("Player 2 Turn")
     }
-    
+
     function changeTurn() {
         console.log(turn);
         if (turn == "X") turn = "O"
@@ -52,11 +65,49 @@ $("document").ready(function () {
 
     function initializeBoard() {
         board = [
-            [null, null, null],
-            [null, null, null],
-            [null, null, null]
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
         ]
     }
+
+    function winner(p) {
+        if (p == "X") {
+            $("#turnMessage").text("Player 1 Win")
+        } else if (p =="O") {
+            $("#turnMessage").text("Player 2 Win")
+        }
+        hasWinner = p
+    }
+
+    function checkWinner() {
+        if (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O"
+        || board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O"
+        || board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O"
+        || board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O"
+        || board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O"
+        || board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O"
+        || board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O"
+        || board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
+        {
+            winner("O")
+        }
+
+        if (board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X"
+        || board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X"
+        || board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X"
+        || board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X"
+        || board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X"
+        || board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X"
+        || board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X"
+        || board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X")
+        {
+            winner("X")
+        }
+
+    }
+
+
     function updateBoard() {
         $("#00").text(board[0][0]);
         $("#01").text(board[0][1]);
