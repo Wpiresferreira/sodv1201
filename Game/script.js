cf036093957c8d6642c0c1c6663e98837103a03d
Wagner Pires Ferreira <125540650+Wpiresferreira@users.noreply.github.com>
2023-09-03 03:01:41 +0000

    version0.5

--- Game/script.js
+++ Game/script.js
@@ -1,27 +1,37 @@
-let monsters = ["img\\angrymonster.png",
-"img\\angrymonster1.png",
-"img\\sadmonster.png"
+let monsters = ["img/angrymonster.png",
+"img/angrymonster1.png",
+"img/scaredmonster.webp",
+"img/smilemonster.png",
+"img/sadmonster.webp",
+"img/sickmonster.webp",
 ]
 let points = 0
 
-$("#points")
 
-setTimeout(test(),1000)
-setTimeout(test(),2000)
-setTimeout(test(),3000)
-setTimeout(test(),4000)
-setTimeout(test(),5000)
 
+for (let index=0; index<10; index++){
+} 
+
+let timeOut = setInterval(() => {
+    test()
+}, 1000); 
 
 function test(){
     
     let sorted = Math.floor(Math.random() * 6 ) + 1;
+    let sortedMonster = Math.floor(Math.random() * 5 );
     console.log(sorted)
     $("#monster" + sorted).click(function(){
-        $(this).html('<img src="img/hitedmonster.png" alt="" srcset="">')
+        $(this).html('<img src="img/hitedmonster1.png" alt="" srcset="">')
     });
+    $("#monster" + sorted).html('<img src="'+ monsters[sortedMonster] + '" alt="" srcset="">')
+    
+    console.log(monsters[sortedMonster])
+
     $("#monster" + sorted).animate({marginTop: "0px"});
-    $("#monster" + sorted).animate({marginTop: "100px"});
+    let wait = setTimeout(() => {
+        $("#monster" + sorted).animate({marginTop: "100px"})
+    }, 1000)
 }
 function test1(){
     $("#monster1").animate({marginTop: "100px"});
